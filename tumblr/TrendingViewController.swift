01fcd532fabb8928e16362941f78966a21bcee74
Dannyboy414 <dannyboy414@gmail.com>
2016-02-25 17:12:47 -0800

    Initial View controllers (tab, home, search, compose, account. trending) setup with assets and buttons.

---
+++ tumblr/TrendingViewController.swift
@@ -0,0 +1,38 @@
+//
+//  TrendingViewController.swift
+//  tumblr
+//
+//  Created by Daniel Kim on 2/25/16.
+//  Copyright © 2016 Daniel Kim. All rights reserved.
+//
+
+import UIKit
+
+class TrendingViewController: UIViewController, UIScrollViewDelegate {
+    @IBOutlet weak var scrollView: UIScrollView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        scrollView.delegate = self
+        self.scrollView.contentSize = CGSize(width: 320, height: 1217)
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
